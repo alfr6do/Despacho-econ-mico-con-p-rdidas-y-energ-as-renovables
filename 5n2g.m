46210242e0980ae23ccbc23d16d3febc90d4a362
alfr6do <alfredo.vel1996@gmail.com>
2020-04-15 14:31:24 -0500

    Update 5n2g.m

--- 5n2g.m
+++ 5n2g.m
@@ -1,7 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Datos del sistema
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%NOTA IMPORTANTE---- Nodo 1 siempre como nodo Slack
+Base_MVA=100;
+
       %Nodo   Tipo  V(pu)    Ang(radiantes)  P_gen(pu)  Q_gen(pu)   P_d(pu) Q_d(pu) Vmin Vmax
 V_nod=[  1     1     1.06        0           1.3112     0.9082      0.0     0.0      0.9  1.1;
          2     2      1       -0.0360         0.4      -0.6159      0.2     0.1      0.9  1.1;
@@ -21,13 +22,7 @@ Dat= [1     2	    0.02	   0.06     0	  0.06      2        1;
       3     4	    0.01       0.03     0	  0.02      2        1;
       4     5	    0.08       0.24     0	  0.05      2        1;
               ];
-Base_MVA=100;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Num_Lin = size(Dat,1); %numero de lineas
-Num_Nod = size(V_nod,1); %numero de buses
-De     = Dat(:,1); 
-Hacia  = Dat(:,2);
-[Y] = Ybus(Num_Nod,Num_Lin,De,Hacia,Dat);
 %Curvas de costos de generacion
 % C(Pg_i) = a_i + b_i*Pg_i + d_i*(Pg_i)^2
 %Col 1 = a_i
